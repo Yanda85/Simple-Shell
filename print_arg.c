8dc08b0557b1f655bbb6550a17ffe966884cfc84
Amanda1554 <129445007+Amanda1554@users.noreply.github.com>
2023-08-21 13:04:58 +0200

    Create print_arg.c

---
+++ print_arg.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - prints all arguments
+* @c: number of arguments in av
+* @av: array of strings (arguments)
+*/
+
+it main(int ac, char **av)
+{
+  int index;
+
+for(index = 0; index < ac; index++)
+{
+(void) ac;
+printf("%s\n", av[index]);
+}
+return (0);
+}
