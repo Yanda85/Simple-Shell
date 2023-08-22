a4d82d194a7946d5571b717e03178f07083f3d3f
Amanda1554 <129445007+Amanda1554@users.noreply.github.com>
2023-08-22 16:44:35 +0200

    Update toem_atoi.c

--- toem_atoi.c
+++ toem_atoi.c
@@ -35,3 +35,29 @@ return (0);
 
 int _isalpha(int c)
 {
+  if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+    return (0);
+else
+    return (0);
+}
+
+/**
+* _atoi - converts a string argument to an interger
+* @c: character to be used
+* Return: 1 if successful, 0 if unsuccessful
+*/
+
+int _atoi(char *c)
+{
+  int atoi = 0;
+int i = 0;
+
+if (str[i] == '-')
+  i++;
+
+while (str[i] >= '0' && str[i] <= '9')
+{
+atoi = atoi * 10 + (str[i] - '0');
+i++;
+}
+return (result);
