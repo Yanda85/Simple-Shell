c3b1610a676c09004a9b005d5a3f48ff0710dc28
Amanda1554 <129445007+Amanda1554@users.noreply.github.com>
2023-08-24 18:21:12 +0200

    Update shell.h

--- shell.h
+++ shell.h
@@ -125,9 +125,12 @@ char *find_path(info_t *, char *, char *);
 /* loophsh.c */
 int loophsh(char **);
 
+/* main.c */
+int main.c(int ac, char **av)
+
 /* toem_errors.c */
-void _eputs(char *);
-int _eputchar(char);
+void _eputs(char *str);
+int _eputchar(char c);
 int _putfd(char c, int fd);
 int _putsfd(char *str, int fd);
 
