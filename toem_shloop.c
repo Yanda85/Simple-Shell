48b6bdeea5b7e4e67be1a9082dae7d2e72a9f345
Amanda1554 <129445007+Amanda1554@users.noreply.github.com>
2023-08-24 19:57:54 +0200

    Update toem_shloop.c

--- toem_shloop.c
+++ toem_shloop.c
@@ -113,3 +113,41 @@ print_error(info, "not found\n");
 }
 }
 }
+
+/**
+* fork_cmd - thread to run cmd
+* @infoz; info structure
+* Return: nothing
+*/
+
+void fork_cmd(info_t *info)
+{
+  pid_t child_pid;
+
+child_pid = fork();
+if (child_pid == -1)
+{
+perror("Error:");
+return;
+}
+if (child_pid == 0)
+{
+if (execve(info->path, info->argv, get_environ(info)) == -1)
+{
+free_info(info, 1);
+if (errno == EACCESS)
+  exit(126);
+exit(1);
+}
+}
+else
+{
+wait(&(info->status));
+if (WIFEXITED(info->status))
+{
+info->status = WEXITSTATUS(info->status);
+if (info->status == 126)
+  print_error(info, "Permission denied\n");
+}
+}
+}
