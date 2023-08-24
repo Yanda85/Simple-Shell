fa910ab33840161fb1a4ee07d5f8b2bd9d0af431
Amanda1554 <129445007+Amanda1554@users.noreply.github.com>
2023-08-24 18:09:47 +0200

    Update shell.h

--- shell.h
+++ shell.h
@@ -183,9 +183,11 @@ int _myhistory(info_t *info);
 int _myalias(info_t *info);
 
 /*toem_getline.c */
-ssize_t get_input(info_t *);
-int _getline(info_t *, char **, size_t *);
-void sigintHandler(int);
+ssize_t input_buf(info_t *info, char **buf, size_t *len)
+ssize_t get_input(info_t *info);
+ssize_t read_buf(info_t *info, char *buf, size_t *i)
+int _getline(info_t *info, char **ptr, size_t *length);
+void sigintHandler(int sig_num);
 
 /* toem_getinfo.c */
 void clear_info(info_t *);
